c8d21200eada955e6837fa1b25673ced3b28a585
ThiagoMonteiroBSI <thiagodepaulamonteiro@gmail.com>
2025-10-07 10:56:51 -0300

    AXIOS

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+const produtos = ref([])
+
+async function buscarProdutos() {
+   const response = await axios.get('/produtos')
+   produtos.value = response.data
+}
+
+</script>
+
+<template>
+<h1> Listagem de Produtos </h1>
+<ul>
+    <li v-for="produto in produtos" :key="produto.id">
+        {{ produto.nome }} - R$ {{ produto.preco }}
+    </li>
+</ul>
+<button @click="buscarProdutos">Buscar</button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/plugins/axios.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+const apiUrl = import.meta.env.VITE_API_URL
+
+axios.defaults.baseURL = apiUrl
+
